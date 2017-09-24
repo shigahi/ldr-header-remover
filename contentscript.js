18c3596e4269f6ce0e8033acd795ab9aba142353
masanari.higashi@classmethod.de <29591085+shigahi@users.noreply.github.com>
2017-09-25 01:07:15 +0200

    refresh

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,3 @@
-function(){
-  var ldrHeader = document.getElementsByTagName("div");  // ページから divタグの配列を取得して変数に定義。
+var ldrHeader = document.getElementsByTagName("div");  // ページから divタグの配列を取得して変数に定義。
   ldrHeader[0].style = "";  // 配列の先頭が該当するお知らせヘッダーなので 0 で指定し、そのstyleプロパティを空にする。
   ldrHeader[0].innerHTML = ""  // 同様に<div></div>に囲まれた部分を innerHTML プロパティで取得して空にする。
-};
